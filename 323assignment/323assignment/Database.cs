e3d87bf2ea3959f41a70f8465872ff10c78a1de9
Coplestone, Jack Liam <jack.cop2000@gmail.com>
2021-05-25 20:37:21 +1200

    Initial Solution commit

---
+++ 323assignment/323assignment/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace _323assignment
+{
+    class Database
+    {
+        private void Connect()
+        {
+            string oradb = "Data Source=ORCL;User Id=hr;Password=hr;";
+            OracleConnection conn = new OracleConnection(oradb);  // C#
+            conn.Open();
+        }
+    }
+}
